118f7ab20e4fbe8cbb2c98795c2a1b3ac2b3de0a
Louis Falk Knudsen <louis@falkknudsen.com>
2026-02-14 21:46:17 +0100

    Restructured the configuration/argument parsing to output an immutable struct, and implemented the remaining missing arguments' functionality.

--- Assignments/02/Arguments.fs
+++ Assignments/02/Arguments.fs
@@ -1,88 +1,157 @@
 module a02.Arguments
 
-/// Struct for holding command-line argument information.
-type Config() =
+open System
+
+/// Encapsulates parameter information for printing help information.
+/// More readable than (char * string * string).
+type private Param =
+    struct
+        /// The single-character initial (e.g. v)
+        val short : char
+        /// The long-form parameter name excl. leading dashes (e.g. verbose)
+        val long : string
+        /// The help-description for what this parameter does.
+        val desc : string
+
+        new (short, long, desc) = {
+                short = short;
+                long = long;
+                desc = desc
+        }
+
+        override this.ToString() =
+            "\t-" + string this.short + "\n\t--" + string this.long + ":\t\t" + string this.desc + "\n"
+    end
+
+type private Params =
+    class
+        static member parameters = [
+            Param('c', "continue",
+                "Continue when assertions fail.")
+            Param('b', "break",
+                "Stop if an assertion fails and print the exception message.")
+            Param('v', "verbose",
+                "Show the results of the individual assertions.")
+            Param('q', "quiet",
+                "Override all other options, and print nothing.")
+            Param('l', "list",
+                "Print a list of the failed test cases at the end.")
+            Param('f', "failures",
+                "Only show failed test cases. Implies -v.")
+            Param('h', "help",
+                "Print this help text and exit.")
+        ]
+
+        /// Pretty print usage information.
+        static member printHelp =
+            Console.WriteLine("Usage: dotnet run -- [<Options>...]\n\nOptions (all case-insensitive):\n")
+
+            let rec _printParameters (parameters : Param list) =
+                match parameters with
+                | [] -> ()
+                | head :: tail ->
+                    Console.WriteLine(head)
+                    _printParameters tail
+
+            _printParameters Params.parameters
+
+        static member printInvalidArgument arg =
+            Console.WriteLine("Invalid argument '" + arg +
+                              "'\nExecute with argument -h or --help to see usage information.")
+    end
+
+/// Data structure for holding command-line argument information.
+type private ConfigBuilder() =
     class
         let mutable _continueOnFailure = true
         let mutable _verbose = true
+        let mutable _quiet = false
         let mutable _printFailedTestIndices = false
         let mutable _showOnlyFailures = false
 
+        let _parse (args : string array) =
+            let upper = args.Length - 1
+            for i = 0 to upper do
+                match args[i].ToLower() with
+                    | "-c"
+                    | "--continue" ->   _continueOnFailure <- true
+                    | "-b"
+                    | "--break" ->      _continueOnFailure <- false
+                    | "-v"
+                    | "--verbose" ->    _verbose <- false
+                    | "-q"
+                    | "--quiet" ->      _quiet <- true
+                    | "-l"
+                    | "--list" ->       _printFailedTestIndices <- true
+                    | "-f"
+                    | "--failures" ->   _showOnlyFailures <- true
+                    | "-h"
+                    | "--help" ->
+                        Params.printHelp
+                        exit 0
+                    | _ ->
+                        Params.printInvalidArgument args[i]
+                        exit -1
+
+        member private this.setFields (args : string array) =
+            _parse args
+            this
+
+        /// If true, a failed assertion does not throw an exception.
+        /// Default is true. Override with --break or -b.
         member this.continueOnFailure = _continueOnFailure
+
+        /// If true, print all results, not just the cumulative final results.
+        /// Default is true. Override with --verbose or -v.
         member this.verbose = _verbose
-        member this.printFailedTestNumbers = _printFailedTestIndices
+
+        /// If true, print absolutely nothing, overriding any other flags.
+        /// Default is false. Override with --quiet or -q.
+        member this.quiet = _quiet
+
+        /// If true, end by printing a list of test numbers which failed.
+        /// Defaults to false. Override during execution with -l
+        member this.printFailedTests = _printFailedTestIndices
+
+        /// Print only the results of tests which failed.
+        /// Implies -v.
+        /// Defaults to false. Override during execution with -f.
         member this.showOnlyFailures = _showOnlyFailures
 
+        /// Parse command-line arguments and return the result as a new Config instance.
+        /// If -h or --help is encountered, will print usage information and exit with
+        /// code 0.
+        /// If an unrecognised argument is encountered, will print an error message and
+        /// exit with code -1.
+        static member parse(args : string array) : ConfigBuilder =
+            if args.Length > 0 then
+                ConfigBuilder().setFields(args)
+            else
+                ConfigBuilder()
 
-        member this.setContinue b =
-            _continueOnFailure <- b
-            this
-
-        member this.setVerbose b =
-            _verbose <- b
-            this
-
-        member this.setPrintFailedTestNumbers b =
-            _printFailedTestIndices <- b
-            this
-
-        member this.setShowOnlyFailures b =
-            _showOnlyFailures <- b
-            this
     end
 
-let parameters (config: Config) =
-    [ ('c', "continue", config.setContinue, true, "Continue when assertions fail.")
-      ('b',
-       "break",
-       config.setContinue,
-       false,
-       "Stop if an assertion fails and print the exception message.")
-      ('v', "verbose", config.setVerbose, true, "Show the results of the individual assertions.")
-      ('q', "quiet", config.setVerbose, false, "Show only the cumulative results.")
-      ('l',
-       "list",
-       config.setPrintFailedTestNumbers,
-       true,
-       "Print a list of the failed test cases at the end.") ]
+/// Data structure for holding command-line argument information.
+type Config =
+    struct
+        val continueOnFailure : bool
+        val verbose : bool
+        val quiet : bool
+        val printFailedTestNumbers : bool
+        val showOnlyFailures : bool
 
-let rec _printParameters (parameters: ('a * 'b * 'c * 'd * 'e) list) =
-    match parameters with
-    | [] -> ()
-    | (alias, command, _, _, description) :: tail ->
-        System.Console.WriteLine(
-            "-" + string alias + "\n" + "--" + string command + ":\t\t" + description
-        )
+        private new (c : ConfigBuilder) =
+            {
+                continueOnFailure = c.continueOnFailure
+                verbose = c.verbose
+                quiet = c.verbose
+                printFailedTestNumbers = c.printFailedTests
+                showOnlyFailures = c.showOnlyFailures
+            }
 
-        _printParameters tail
+        new (args : string array) =
+            Config(ConfigBuilder.parse args)
 
-let printHelp config =
-    System.Console.WriteLine("Command line arguments:\n")
-    _printParameters (parameters config)
-    config
-
-
-/// Parse command-line arguments and return the result as a Config struct.
-let rec _parseArgs args config : Config =
-    match args with
-    | [] -> config
-    | head :: tail ->
-        match head with
-        | "-c"
-        | "--continue" -> _parseArgs tail (config.setContinue true)
-        | "-b"
-        | "--break" -> _parseArgs tail (config.setContinue false)
-        | "-v"
-        | "--verbose" -> _parseArgs tail (config.setVerbose true)
-        | "-q"
-        | "--quiet" -> _parseArgs tail (config.setVerbose false)
-        | "-l"
-        | "--list" -> _parseArgs tail (config.setPrintFailedTestNumbers true)
-        | "-f"
-        | "--failures" -> _parseArgs tail (config.setShowOnlyFailures true)
-        | "-h"
-        | "--help" -> printHelp config
-        | _ -> _parseArgs tail config
-
-/// Parse command-line arguments and return the result as a Config struct.
-let parseArgs args = _parseArgs args (Config())
+        static member parse (args : string array) =
+            Config(args)
+    end
