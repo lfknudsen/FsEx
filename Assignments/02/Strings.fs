c1db5cea1946c029afe93ab03048885618454d59
Louis Falk Knudsen <louis@falkknudsen.com>
2026-02-14 22:32:27 +0100

    Optimised prefixSpace a bit.

--- Assignments/02/Strings.fs
+++ Assignments/02/Strings.fs
@@ -1,17 +1,23 @@
 (* A few utility functions. *)
 module a02.Strings
 
-/// Returns 's' as a string prefixed with spaces so that the result is at least
-/// 'width' characters long.
-let prefixSpace s (width: int) =
-    let str = string s
+open System
 
-    if width <= 0 || str.Length >= width then
-        str
+/// Returns 'message' as a string prefixed with spaces so that the result is at least
+/// 'width' characters long.
+/// The prefix string of spaces is interned.
+let prefixSpace (message: 'a) (width: int) =
+    let stringVersion = string message
+
+    if width <= 0 || stringVersion.Length >= width then
+        stringVersion
     else
-        let diff = width - str.Length
-        let prefix = String.init diff (fun _ -> " ")
-        prefix + str
+        let diff = width - stringVersion.Length
+        if diff <= 0 then
+            stringVersion
+        else
+            let prefix = String.Intern(String.replicate diff " ")
+            prefix + stringVersion
 
 let rec _formatList lst delimiter =
     match lst with
@@ -20,7 +26,7 @@ let rec _formatList lst delimiter =
     | [ first; last ] -> string first + delimiter + string last
     | head :: neck :: tail -> string head + delimiter + string neck + _formatList tail delimiter
 
-/// Prints the full contents of a list.
+/// Formats the full contents of a list for printing.
 /// 'delimiter' refers to the string that will be placed in-between elements,
 /// such as ", " or "\n".
 let formatList lst delimiter =
