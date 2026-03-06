9ab288577ac2b3e1729a43ec13a2f803d0afafe3
Louis <louis@falkknudsen.com>
2026-03-06 19:34:31 +0100

    Added first tests for green questions in assignment 5.

---
+++ Assignments/05/Tests.fs
@@ -0,0 +1,53 @@
+﻿module Tests
+
+open Interpreter.State
+open System
+open Xunit
+
+[<Fact>]
+let TestReservedVariableName () =
+    Assert.False(reservedVariableName "hello")
+    Assert.True(reservedVariableName "if")
+    Assert.True(reservedVariableName "then")
+    Assert.True(reservedVariableName "else")
+    Assert.True(reservedVariableName "while")
+    Assert.True(reservedVariableName "declare")
+    Assert.True(reservedVariableName "print")
+    Assert.True(reservedVariableName "random")
+    Assert.True(reservedVariableName "fork")
+    Assert.True(reservedVariableName "__result__")
+    Assert.False(reservedVariableName "")
+    Assert.False(reservedVariableName "i")
+
+[<Fact>]
+let TestValidVariableName () =
+    Assert.True(validVariableName "hello")
+    Assert.True(validVariableName "_hello")
+    Assert.True(validVariableName "_1hello")
+    Assert.False(validVariableName "1_hello")
+    Assert.False(validVariableName "1hello")
+    Assert.True(validVariableName "h")
+    Assert.False(validVariableName "")
+
+[<Fact>]
+let TestState () =
+    Assert.Equal<int option>(None, () |> mkState |> getVar "x")
+    Assert.Equal<int option>(Some 0, () |> mkState |> declare "x" |> Option.bind (getVar "x"))
+
+    Assert.Equal<int option>(
+        Some 42,
+        ()
+        |> mkState
+        |> declare "x"
+        |> Option.bind (setVar "x" 42)
+        |> Option.bind (getVar "x")
+    )
+
+    Assert.Equal<int option>(
+        None,
+        ()
+        |> mkState
+        |> declare "1x"
+        |> Option.bind (setVar "1x" 42)
+        |> Option.bind (getVar "1x")
+    )
