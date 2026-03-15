a716afc02b90804ff4a396b570b9b3213b1fb367
Louis Falk Knudsen <louis@falkknudsen.com>
2026-03-15 17:57:17 +0100

    Implemented the tests in the description (except with Result instead of Option).

--- Assignments/06/TestsMemory.fs
+++ Assignments/06/TestsMemory.fs
@@ -1,31 +1,97 @@
 module _06.TestsMemory
 
-open System.Collections.Generic
+open Interpreter.Eval
+open Interpreter.Language
+open Interpreter.State
 open Xunit
 open Interpreter.Memory
 
 [<Fact>]
-let EmptyMemory () =
-    let mem = empty 10
-    Assert.Equal(0, mem.next)
-    Assert.Equal(0, mem.heap.Count)
+let TestEmpty () =
+    let mem = (empty 0 |> alloc 0)
+    Assert.Equal(Error (NegativeMemoryAllocated 0), mem)
+
+let create size : state =
+    mkState size
 
 [<Fact>]
-let TestAlloc () =
-    let allocation = empty 10 |> alloc 20
-    Assert.True(allocation.IsSome)
+let TestFreePtrLowerThanNextPtrPlusSizeGreaterThanNext () =
+    let size = 20
+    let st = create size
+    let freed = Interpreter.State.free 10 30 st
+    Assert.True(freed.IsError)
 
-    let mem, next = allocation.Value
-    Assert.Equal(20, mem.next)
-    Assert.Equal(mem.next, next)
-    Assert.Equal(20, mem.heap.Count)
+[<Fact>]
+let AllocMemRead () =
+    let state = create 10
+    let result =
+        state
+        |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3), Seq(Declare "result",
+            Assign ("result",
+            MemRead (Var "x"))))))
+        |> Result.bind (getVar "result")
 
-    Assert.All(mem.heap, (fun (kv : KeyValuePair<int,int>) -> Assert.Equal(kv.Value, 0)))
+    Assert.Equal(Ok 0, result)
 
-[<Theory>]
-[<InlineData(-3)>]
-[<InlineData(0)>]
-[<InlineData(-1)>]
-let FailToAlloc (size : int) =
-    let allocation = empty 10 |> alloc size
-    Assert.Equal(None, allocation)
+[<Fact>]
+let Alloc2 () =
+    let result =
+        create 10
+        |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3),
+                                        Seq(MemWrite(Var "x", Num 42), Seq(Declare "result",
+                                                                           Assign("result", MemRead (Var "x")))))))
+        |> Result.bind (getVar "result")
+    Assert.Equal(Ok 42, result)
+
+[<Fact>]
+let Alloc3 () =
+    let result = 10
+                 |> mkState
+                 |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3), Seq(MemWrite(Var "x", Num
+                        42), Seq(Declare "result", Assign("result", MemRead (Var "x" .+. Num 1)))))))
+                 |> Result.bind (getVar "result")
+    Assert.Equal(Ok 0, result)
+
+[<Fact>]
+let Alloc4 () =
+    let result = 10 |> mkState
+                 |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3), Seq(MemWrite(Var "x", Num
+                        42), Seq(Declare "result", Assign("result", MemRead (Var "x" .+. Num 3)))))))
+                 |> Result.bind (getVar "result")
+
+    Assert.Equal<Result<int,error>>(Error (MemoryNotAllocated 3), result)
+
+[<Fact>]
+let Alloc5 () =
+    let result = 10 |> mkState
+                 |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3), Seq(MemWrite(Var "x", Num
+                        42), Seq(Free(Var "x" .+. Num 1, Num 2),
+                                 Seq(Declare "result",
+                                     Assign("result", MemRead (Var "x"))))))))
+                 |> Result.bind (getVar "result")
+
+    Assert.Equal<Result<int,error>>(Ok 42, result)
+
+
+[<Fact>]
+let Alloc6 () =
+    let result = 10 |> mkState
+                 |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3),
+                                                     Seq(MemWrite(Var "x", Num 42),
+                                                         Seq(Free(Var "x", Num 2),
+                                 Seq(Declare "result",
+                                     Assign("result", MemRead (Var "x"))))))))
+                 |> Result.bind (getVar "result")
+
+    Assert.Equal<Result<int,error>>(Error (MemoryNotAllocated 0), result)
+
+
+[<Fact>]
+let Alloc7 () =
+    let result = 10 |> mkState
+                 |> stmntEval (Seq (Declare "x", Seq(Alloc ("x", Num 3),
+                                                     Seq(MemWrite(Var "x", Num 42),
+                                                         Free(Var "x", Num 4)))))
+                 |> Result.bind (getVar "result")
+
+    Assert.Equal<Result<int,error>>(Error (MemoryNotAllocated 3), result)
